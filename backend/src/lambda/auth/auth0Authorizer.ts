6a5f7dda617189cfa47a2a3d7cc8eef2d573e67f
ThanhPM2 <minhthanh110998@gmail.com>
2022-12-11 23:03:44 +0700

    add config

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -11,7 +11,7 @@ const logger = createLogger('auth')
 // TODO: Provide a URL that can be used to download a certificate that can be used
 // to verify JWT token signature.
 // To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = 'https://dev-m995nk3t.auth0.com/.well-known/jwks.json'
+const jwksUrl = 'https://dev-1703pmt.us.auth0.com/.well-known/jwks.json'
 
 export const handler = async (
   event: CustomAuthorizerEvent
