ae5c77987f50721031d6aa556a32c45b07bf0820
ThanhPM <>
2022-12-12 21:55:51 +0700

    redeploy and update config for fe site

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,11 +1,11 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'wv28ydvtl8'
+const apiId = 'zlald1e2xa'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map. For example:
   // domain: 'dev-nd9990-p4.us.auth0.com',
-  domain: 'dev-m995nk3t.auth0.com',            // Auth0 domain
-  clientId: 'III0JFYWpr1Ohu9VQO8ts60djVQ8XaVW',          // Auth0 client id
+  domain: 'dev-1703pmt.us.auth0.com',            // Auth0 domain
+  clientId: 'wLnjDMQjbomvHMCBpoQkaRsTDrHfkzsH',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
